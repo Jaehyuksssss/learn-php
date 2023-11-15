37641f5b473d0575a54b6af89d0e3e62fe09a740
Jaehyuksssss <ljh95310@naver.com>
2023-11-15 15:32:03 +0900

    php sample2

--- sample2.php
+++ sample2.php
@@ -1,1 +1,9 @@
 <?php
+
+$name = 'Jaehyuk'; // 문자열 변수
+$age = 29; // 숫자 변수
+$job = 'programmer'; // 문자열 변수
+$address='Seoul'; // 문자열 변수
+echo $name."의 나이는 ".$age."살이고 직업은 ".$job."이고 사는 곳은 ".$address."입니다.";
+
+?>
